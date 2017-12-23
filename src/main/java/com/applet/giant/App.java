985f10f043b6284e8017b303d0b2fd33e5baf6e9
wangdaguang <wdg@foisonagel.com>
2017-12-23 16:06:28 +0800

    小程序项目

---
+++ src/main/java/com/applet/giant/App.java
@@ -0,0 +1,13 @@
+package com.applet.giant;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
